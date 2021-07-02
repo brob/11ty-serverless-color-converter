6491cffd819183de74bae6ad5ef78f25465e7c89
Bryan Robinson <bryan@sanity.io>
2021-07-02 11:07:54 -0400

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,29 @@
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+const convert = require('color-convert');
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: "color", // The serverless function name from your permalink object
+    functionsDir: "./netlify/functions/",
+  });
+
+  const colorSpaces = [
+      "rgb", "hsl", "hex", "lch"
+  ]
+  eleventyConfig.addFilter("convert", function(color,type) { 
+
+      const conversions = colorSpaces.map(space => {
+        const doWrite = type != space
+        const conversion = doWrite ? convert[type][space](color) : ''
+        return doWrite ? `<li>
+        ${space}: <code>${conversion}</code>
+        </li>
+    ` : ''}).join('')
+      
+      
+      return `<ul>
+        ${conversions}
+      </ul>`
+    });
+
+};
